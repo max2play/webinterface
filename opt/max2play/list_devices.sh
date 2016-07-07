0e156d0e2b64b4c0ee68ede3504ad0c739823e35
Max2Play <stefan@netzberater.de>
2016-07-07 16:37:33 +0200

    Improved Network search, if hostnames not resolved by router

--- opt/max2play/list_devices.sh
+++ opt/max2play/list_devices.sh
@@ -18,13 +18,17 @@ if [ -z "$IP" ]; then
         echo `date +"%Y-%m-%d %H:%M"` > /opt/max2play/cache/list_devices.txt
 else        
         SUBNET=${IP%.*}
-        OUTPUT=`nmap -p 80 --open $SUBNET.* | grep -o -e "[a-zA-Z0-9\.]\+ ([0-9\.]\+)$" | sed 's/ (/|/;s/)//'`
+        OUTPUT=`nmap -p 80 --open $SUBNET.* | grep -o -e "\([a-zA-Z0-9\.]\+ ([0-9\.]\+)\)\|\([0-9\.]\{6,\}\)$" | sed 's/ (/|/;s/)//'`
         FILEWRITE=`date +"%Y-%m-%d %H:%M"`
         # Check for Max2Play Webinterface
         while read -r line ; do
           ADDRESS=$(echo "$line" | sed 's/^.*|//')
           CHECK=$(wget -T 3 -t 1 -q -O - "$@" $ADDRESS/style.css | grep WUoqrET9fUeobQW7jkRT8E0i7KZn-EPnyo3HZu7kw | wc -l)
           if [ "$CHECK" -gt "0" ]; then
+                if [ "$(echo $line | grep \| | wc -l)" -lt "1" ]; then
+                	# Add | as the host has no hostname
+                	line="|$line"
+                fi 
                 if [ "$ADDRESS" = "$IP" ]; then
                 	FILEWRITE="$FILEWRITE\n$line|current"
                 else
